4d0f81dd837e94c352f419791003868c1bae0b3d
Reethika Ambatipudi <94963211+Reethikaraj@users.noreply.github.com>
2022-11-29 22:24:23 +0100

    Create main.py

---
+++ day16/main.py
@@ -0,0 +1,20 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+isOn = True
+while isOn:
+    choice = input(f'What would you like to have? {menu.get_items()}: ')
+    if choice == 'off':
+        isOn = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
